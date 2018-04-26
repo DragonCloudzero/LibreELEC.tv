bb1be783b954f1f903b0dde43941ad5cd38f6d06
cvh <namerp@googlemail.com>
2018-04-26 14:40:08 +0200

    hauppauge: updated patches to latest

--- packages/linux-driver-addons/dvb/hauppauge/package.mk
+++ packages/linux-driver-addons/dvb/hauppauge/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="hauppauge"
-PKG_VERSION="f5a5e5e"
-PKG_SHA256="6a3167c9990fa96838f4746861edb4d4e656739ea08d4f993e54becb9f2e9ab2"
+PKG_VERSION="f334f01"
+PKG_SHA256="f7ccf652d90a51bd709f9d84a286a4e468c62fe50dc216473eb43ffdc187b0a9"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.linuxtv.org/media_build.git"
